301e55d5312a76e13c80912103739c4ccd5c44f0
Rudy <rudy.berends@visolity.nl>
2026-01-18 11:45:28 +0000

    build: link ALSA include paths via pkg-config

--- build.rs
+++ build.rs
@@ -2,8 +2,14 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         println!("cargo:rerun-if-changed=src/alsa_silence.c");
-        cc::Build::new()
-            .file("src/alsa_silence.c")
-            .compile("alsa_silence");
+        let alsa = pkg_config::Config::new()
+            .probe("alsa")
+            .expect("alsa development files not found (pkg-config)");
+        let mut build = cc::Build::new();
+        build.file("src/alsa_silence.c");
+        for path in alsa.include_paths {
+            build.include(path);
+        }
+        build.compile("alsa_silence");
     }
 }
